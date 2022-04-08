0af6c3d7428611f492e247a292e932b0ebc0dc5a
bartekmajster <bkochanovitz@gmail.com>
2022-04-08 15:10:03 +0200

    setup styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Root } from 'Views/Root';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'Styles/theme';
 
 const App: FC = (): JSX.Element => (
   <BrowserRouter>
-    <Root />
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
   </BrowserRouter>
 );
 
