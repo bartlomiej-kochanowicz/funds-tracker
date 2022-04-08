e2ff2161ff4dbd2abef6a222eaca4952b0faa8dd
bartekmajster <bkochanovitz@gmail.com>
2022-04-08 09:38:01 +0200

    configure eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,19 @@
-import { useState } from "react";
-import { Switch } from "antd";
-import logo from "./logo.svg";
-import "antd/dist/antd.css";
-import "./App.css";
+import { Switch } from 'antd';
+import 'antd/dist/antd.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  function onChange(checked: boolean) {
+const App = () => {
+  const onChange = (checked: boolean) => {
     console.log(`switch to ${checked}`);
-  }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-
-        <Switch defaultChecked onChange={onChange} />
-
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Switch
+        defaultChecked
+        onChange={onChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
