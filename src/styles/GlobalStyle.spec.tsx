c602d9e80e5eb2eae41dba662b8137d74ad2335b
bartekmajster <bkochanovitz@gmail.com>
2022-05-13 15:03:31 +0200

    GlobalStyles tests

---
+++ src/styles/GlobalStyle.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from 'utils/test-utils';
+import { GlobalStyle } from 'styles/GlobalStyle';
+
+describe('Styles/GlobalStyle tests', () => {
+  it('renders properly', () => {
+    render(<GlobalStyle />);
+  });
+});
