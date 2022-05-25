027540424362a136ecbe69d68bd4655495b08106
bartekmajster <bkochanovitz@gmail.com>
2022-05-25 23:20:02 +0200

    move font to head

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
-
   :root {
     --doc-height: 100%;
   }
