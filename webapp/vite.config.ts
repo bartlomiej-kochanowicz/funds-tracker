3470a91b7cef15f74997b9e34883258b3feabf92
Bartek Kochanowicz <56134772+bartlomiej-kochanowicz@users.noreply.github.com>
2022-06-23 01:11:31 +0200

    Fix translations files on production (#12)

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,8 +4,26 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import analyze from 'rollup-plugin-analyzer';
 import svgr from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr(), eslint(), analyze()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    eslint(),
+    analyze(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/locales/*',
+          dest: 'locales',
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: './dist',
+  },
 });
