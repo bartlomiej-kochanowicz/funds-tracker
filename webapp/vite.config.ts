01f9ea6e00314b298075a7b7a12ce61db0b0cc9d
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-06-22 20:44:18 +0200

    modify vite config

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -8,4 +8,7 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr(), eslint(), analyze()],
+  build: {
+    outDir: './dist',
+  },
 });
