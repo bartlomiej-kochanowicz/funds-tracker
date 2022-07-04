e89e114bafa6ec8a5821a5abd2745bdd15dd53ca
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-04 22:51:09 +0200

    add input type

--- webapp/src/views/Login/Login.tsx
+++ webapp/src/views/Login/Login.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
 import { Button } from 'components/atoms/Button';
 import { Spacer } from 'components/atoms/Spacer';
 import { Input } from 'components/atoms/Input';
@@ -8,6 +9,8 @@ import { StyledFullscreenClear, Wrapper, Form } from './Login.styles';
 
 export const Login = () => {
   const { t } = useTranslation(['common', 'login']);
+
+  const { handleSubmit } = useFormik({ initialValues: {}, onSubmit: () => {} });
 
   return (
     <StyledFullscreenClear
@@ -29,12 +32,18 @@ export const Login = () => {
 
         <Spacer space="large" />
 
-        <Form>
-          <Input placeholder={t('login:page.login.email.placeholder')} />
+        <Form onSubmit={handleSubmit}>
+          <Input
+            placeholder={t('login:page.login.email.placeholder')}
+            type="email"
+          />
 
           <Spacer />
 
-          <Input placeholder={t('password')} />
+          <Input
+            placeholder={t('password')}
+            type="password"
+          />
 
           <Spacer />
 
