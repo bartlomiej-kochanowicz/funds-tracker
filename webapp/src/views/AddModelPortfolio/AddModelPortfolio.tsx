e64b929932ca213d34177835ff6369ddf259627b
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-05 23:07:20 +0200

    update select

--- webapp/src/views/AddModelPortfolio/AddModelPortfolio.tsx
+++ webapp/src/views/AddModelPortfolio/AddModelPortfolio.tsx
@@ -1,1 +1,31 @@
-export const AddModelPortfolio = () => <div>Add model portfolio</div>;
+// import styled from 'styled-components';
+import { Row } from 'simple-flexbox';
+import { Input, Select, Spreader } from 'components/atoms';
+import instruments from 'constants/selectors/instruments';
+import { useTranslation } from 'react-i18next';
+
+export const AddModelPortfolio = () => {
+  const { t } = useTranslation(['common', 'selectors']);
+
+  const options = instruments.map(({ label, ...rest }) => ({
+    label: t(`selectors:${label}`),
+    ...rest,
+  }));
+
+  return (
+    <div>
+      <form>
+        <Row>
+          <Input />
+
+          <Spreader />
+
+          <Select
+            options={options}
+            placeholder="Select the instrument"
+          />
+        </Row>
+      </form>
+    </div>
+  );
+};
