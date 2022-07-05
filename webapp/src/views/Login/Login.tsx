bc2791ef49a11e84c9ef4fe0e920109740639bed
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-05 19:13:15 +0200

    add login validation

--- webapp/src/views/Login/Login.tsx
+++ webapp/src/views/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
+import { object, string } from 'yup';
 import { Button } from 'components/atoms/Button';
 import { Spacer } from 'components/atoms/Spacer';
 import { Input } from 'components/atoms/Input';
@@ -7,10 +8,19 @@ import { Heading } from 'components/atoms/Heading';
 import { Text } from 'components/atoms/Text';
 import { StyledFullscreenClear, Wrapper, Form } from './Login.styles';
 
+const validationSchema = object().shape({
+  email: string().email('Invalid email').required('Required'),
+  password: string().min(12, 'Too Short!').max(50, 'Too Long!').required('Required'),
+});
+
 export const Login = () => {
   const { t } = useTranslation(['common', 'login']);
 
-  const { handleSubmit } = useFormik({ initialValues: {}, onSubmit: () => {} });
+  const { handleSubmit } = useFormik({
+    initialValues: {},
+    validationSchema,
+    onSubmit: () => {},
+  });
 
   return (
     <StyledFullscreenClear
