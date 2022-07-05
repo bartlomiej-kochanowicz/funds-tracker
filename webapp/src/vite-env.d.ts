cb68b56de1552f2cab4fe296e2ab2192d699e9e6
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-05 20:35:53 +0200

    prepare build

--- webapp/src/vite-env.d.ts
+++ webapp/src/vite-env.d.ts
@@ -4,3 +4,7 @@
 declare module 'mq-polyfill' {
   export default function matchMediaPolyfill(window: Window): void;
 }
+
+declare namespace React {
+  type StatelessComponent<P> = React.FunctionComponent<P>;
+}
