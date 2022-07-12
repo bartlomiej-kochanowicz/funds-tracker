9622258260f4a5c2540c629a76967e639e9dfc83
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-12 22:29:23 +0200

    fix get types

--- webapp/src/utils/get.ts
+++ webapp/src/utils/get.ts
@@ -40,7 +40,7 @@ export const get = <TData, TPath extends string, TDefault = GetFieldType<TData, 
   const value = path
     .split(/[.[\]]/)
     .filter(Boolean)
-    .reduce<GetFieldType<TData, TPath>>((value, key) => (value as any)?.[key], data as any);
+    .reduce<GetFieldType<TData, TPath>>((field, key) => (field as any)?.[key], data as any);
 
   return value !== undefined ? value : (defaultValue as TDefault);
 };
