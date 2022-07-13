71708fee32d180eb978bf353429bb21be92fe7b7
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-13 12:27:01 +0200

    select error

---
+++ webapp/src/components/atoms/Select/Select.styles.tsx
@@ -0,0 +1,56 @@
+import { HTMLProps } from 'react';
+import styled, { css } from 'styled-components';
+import { darken } from 'color2k';
+
+export const StyledButton = styled.button<
+  HTMLProps<Omit<HTMLButtonElement, 'error'>> & { error: boolean }
+>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  width: 100%;
+
+  ${({ theme, error }) => css`
+    border-radius: ${theme.radius.secondary};
+    border: none;
+    background-color: ${theme.colors.lightGray};
+    padding: ${theme.padding.medium};
+    color: ${theme.colors[error ? 'red' : 'black']};
+    outline-color: ${theme.colors[error ? 'red' : 'blue']};
+    font-weight: ${theme.font.weight[500]};
+    border: 2px solid ${theme.colors[error ? 'red' : 'gray']};
+
+    &:focus {
+      background-color: ${darken(theme.colors.lightGray, 0.05)};
+      color: ${theme.colors[error ? 'red' : 'blue']};
+      border: 2px solid ${theme.colors[error ? 'red' : 'blue']};
+    }
+
+    &::placeholder {
+      color: ${theme.colors.gray};
+    }
+  `}
+`;
+
+export const StyledContent = styled.div<{
+  isSelected: boolean;
+}>`
+  color: ${({ theme, isSelected }) => (isSelected ? 'inline' : theme.colors.gray)};
+`;
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: auto;
+`;
+
+export const Error = styled.span`
+  position: absolute;
+  left: 0;
+  bottom: -1.25rem;
+
+  ${({ theme }) => css`
+    font-size: ${theme.font.size['0.75']};
+    color: ${theme.colors.red};
+  `}
+`;
