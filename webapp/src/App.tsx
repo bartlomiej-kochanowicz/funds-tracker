f0b5d36b80dcc68e30d231630993b877e7071322
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-17 10:42:22 +0200

    add reduc & msw

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,18 +1,22 @@
 import { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Root } from 'views/Root';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
 import { GlobalStyle } from 'styles/GlobalStyle';
+import { store } from 'store';
 
 const App: FC = (): JSX.Element => (
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
 
-      <Root />
-    </ThemeProvider>
-  </BrowserRouter>
+        <Root />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
