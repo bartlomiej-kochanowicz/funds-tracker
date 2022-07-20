3e6bf38b6eaba7b82fe1a3fd22573f0ca2339b7d
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-20 23:05:52 +0200

    refactor & new routes

--- webapp/src/views/Login/Login.schema.ts
+++
@@ -1,9 +0,0 @@
-import { object, string } from 'yup';
-
-export const validationSchema = object().shape({
-  userEmail: string().email('page.login.email.invalid').required('page.login.email.required'),
-  userPassword: string()
-    .min(12, 'page.login.password.too_short')
-    .max(50, 'page.login.password.too_long')
-    .required('page.login.password.required'),
-});
