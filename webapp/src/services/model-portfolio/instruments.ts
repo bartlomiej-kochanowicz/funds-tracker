00d392bf1f4565013f2a48bac8cbbeca8362b103
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-20 23:51:40 +0200

    add service

---
+++ webapp/src/services/model-portfolio/instruments.ts
@@ -0,0 +1,16 @@
+import { client } from 'config/client';
+
+export type Instrument = {
+  uuid: string;
+  name: string;
+  type: string;
+  percentage: number;
+  rebalancing: boolean;
+};
+
+export interface InstrumentsResponse {
+  collection: Instrument[];
+}
+
+export const getModelPortfolioInstruments = () =>
+  client.get<InstrumentsResponse>('/model-portfolio/instruments');
