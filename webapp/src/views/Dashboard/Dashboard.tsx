13207c6152cf24a153eb45b2bce56c045fdd9b91
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-07-20 23:17:01 +0200

    add 404 page

--- webapp/src/views/Dashboard/Dashboard.tsx
+++ webapp/src/views/Dashboard/Dashboard.tsx
@@ -1,15 +1,27 @@
 import { FullscreenClear } from 'layouts/FullscreenClear';
 import { Button, Spacer } from 'components/atoms';
+import { Link } from 'react-router-dom';
+import { ROUTES } from 'ROUTES';
 
 export const Dashboard = () => (
   <FullscreenClear
     alignItems="center"
     justifyContent="center"
   >
-    <Button>Go to model portfolio</Button>
+    <Button
+      as={Link}
+      to={ROUTES.MODEL_PORTFOLIO}
+    >
+      Go to model portfolio
+    </Button>
 
     <Spacer />
 
-    <Button>Go to actual portfolio</Button>
+    <Button
+      as={Link}
+      to={ROUTES.ACTUAL_PORTFOLIO}
+    >
+      Go to actual portfolio
+    </Button>
   </FullscreenClear>
 );
--- webapp/src/views/Signin/Signin.styles.tsx
+++ webapp/src/views/Signin/Signin.styles.tsx
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 import { Column } from 'simple-flexbox';
-import { FullscreenClear } from 'layouts/FullscreenClear';
-
-export const StyledFullscreenClear = styled(FullscreenClear)`
-  background: ${({ theme }) => theme.colors.white};
-`;
 
 export const Wrapper = styled(Column)`
   width: 350px;
