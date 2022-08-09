c85ebf53728ea0dc6524a5b69ed4637540aaf5ec
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-09 17:11:13 +0200

    loading component

--- webapp/src/views/ModelPortfolio/ModelPortfolio.tsx
+++ webapp/src/views/ModelPortfolio/ModelPortfolio.tsx
@@ -1,18 +1,43 @@
+import { useEffect } from 'react';
 import { FullscreenClear } from 'layouts/FullscreenClear';
 import { Box } from 'components/atoms';
+import { Loading } from 'components/molecules/Loading';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectInstruments } from 'store/selectors/model-portfolio';
+import { instrumentsThunk } from 'store/thunks/model-portfolio/instrumentsThunk';
+import { useStatus } from 'hooks/useStatus';
+import { AppDispatch } from 'store';
 import { Instruments } from './components/Instruments';
 
-export const ModelPortfolio = () => (
-  <FullscreenClear
-    alignItems="center"
-    justifyContent="center"
-  >
-    <Box
-      backgroundColor="lightGray"
-      p={4}
-      borderRadius="tertiary"
+export const ModelPortfolio = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { data, status, error } = useSelector(selectInstruments);
+
+  const { loading, loaded } = useStatus(status);
+
+  useEffect(() => {
+    dispatch(instrumentsThunk());
+  }, [dispatch]);
+
+  return (
+    <FullscreenClear
+      alignItems="center"
+      justifyContent="center"
     >
-      <Instruments />
-    </Box>
-  </FullscreenClear>
-);
+      <Box
+        backgroundColor="lightGray"
+        p={4}
+        borderRadius="tertiary"
+      >
+        <Loading
+          loading={loading}
+          loaded={loaded}
+          error={error}
+          propsComponent={data}
+          renderComponent={props => <Instruments {...props} />}
+        />
+      </Box>
+    </FullscreenClear>
+  );
+};
