4f2c013cf4bab06aba39d89cc43b08dc57d88585
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-12 16:16:46 +0200

    fix database

--- webapp/src/views/ModelPortfolio/components/Instruments/Instruments.tsx
+++ webapp/src/views/ModelPortfolio/components/Instruments/Instruments.tsx
@@ -1,15 +1,21 @@
+import { Heading } from 'components/atoms';
 import { InstrumentsResponse } from 'services/model-portfolio/instruments';
+import { Column } from 'simple-flexbox';
 import { Instrument } from 'views/ModelPortfolio/components/Instrument';
 import { Ul } from './Instruments.styles';
 
 export const Instruments = ({ collection }: InstrumentsResponse) => (
-  <Ul>
-    {collection.map(({ uuid, ...rest }) => (
-      <Instrument
-        key={uuid}
-        uuid={uuid}
-        {...rest}
-      />
-    ))}
-  </Ul>
+  <Column>
+    <Heading fontSize="1.5">Your model portfolio:</Heading>
+
+    <Ul>
+      {collection.map(({ uuid, ...rest }) => (
+        <Instrument
+          key={uuid}
+          uuid={uuid}
+          {...rest}
+        />
+      ))}
+    </Ul>
+  </Column>
 );
