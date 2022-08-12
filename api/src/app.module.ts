57dab507ce8999418d694907a787e55c499bd21f
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-12 22:28:28 +0200

    generate auth module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { DB_URL } from 'config/database';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(DB_URL), UserModule],
+  imports: [MongooseModule.forRoot(DB_URL), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
