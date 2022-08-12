2504c0d2c611160cf436f6724ccdb8dc4e9de084
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-12 21:34:39 +0200

    create users module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DB_URL } from 'config/database';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(DB_URL)],
+  imports: [MongooseModule.forRoot(DB_URL), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
