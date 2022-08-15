bd06e2807ef95a7a8f3bbb3f35ba971d5e9125c0
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-16 01:07:01 +0200

    add suppressing react dom error

--- webapp/config/tests/setupTests.ts
+++ webapp/config/tests/setupTests.ts
@@ -1,3 +1,17 @@
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
