009d7dcec1b152481fc8847889d80d9be9f6eb47
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-17 23:26:40 +0200

    useStateMachine hook

--- webapp/src/hooks/useStateMachine.ts
+++ webapp/src/hooks/useStateMachine.ts
@@ -1,34 +1,56 @@
+import { useState } from 'react';
 import { Dict } from 'types/mapped-types';
 
 type Transitions<States extends string, Actions extends string> = {
-  [state in States]: {
-    [action in Actions]: States;
+  [state in States]?: {
+    [action in Actions]?: States;
   };
 };
 
 export class StateMachine<States extends string, Actions extends string> {
   constructor(
-    private initial: States,
-    private states: Dict<States>,
-    private actions: Dict<Actions>,
-    private transitions: Transitions<States, Actions>,
+    private readonly initial: States,
+    private readonly states: Dict<States>,
+    private readonly actions: Dict<Actions>,
+    private readonly transitions: Transitions<States, Actions>,
   ) {}
+
+  get getInitialState() {
+    return this.initial;
+  }
+
+  get getStates() {
+    return this.states;
+  }
+
+  get getActions() {
+    return this.actions;
+  }
+
+  get getTransitions() {
+    return this.transitions;
+  }
 }
 
-export const useStateMachine = () => {
-  /* const [currentState, setCurrentState] = useState<StateType>(
-    states.addFirstInstrument as StateType,
-  );
+export const useStateMachine = <States extends string, Actions extends string>(
+  stateMachine: StateMachine<States, Actions>,
+) => {
+  const [currentState, setCurrentState] = useState<States>(stateMachine.getInitialState);
 
-  const transitions = {
-    [states.addFirstInstrument]: {
-      [actions.CHANGE_ADD_FIRST_SUCCESS]: states.addFirstSuccess,
-    },
-  };
-
-  const transition = (state: StateType, action: ActionType): StateType => {
-    const nextState = transitions[state][action] as StateType;
+  const transition = (state: States, action: Actions): States => {
+    const nextState = stateMachine.getTransitions?.[state]?.[action] as States;
 
     return nextState || state;
-  }; */
+  };
+
+  const updateState = (action: Actions) => setCurrentState(state => transition(state, action));
+
+  const compareState = (state: States) => currentState === state;
+
+  return {
+    states: stateMachine.getStates,
+    actions: stateMachine.getActions,
+    updateState,
+    compareState,
+  };
 };
