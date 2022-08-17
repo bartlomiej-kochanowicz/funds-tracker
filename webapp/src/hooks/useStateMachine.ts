980945e481551dfc8a3e3fe4e41e3cde5ea747dc
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-17 23:57:43 +0200

    partial finish signin

--- webapp/src/hooks/useStateMachine.ts
+++ webapp/src/hooks/useStateMachine.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Dict } from 'types/mapped-types';
+
+type Dict<T extends string> = { [k in T]: T };
 
 type Transitions<States extends string, Actions extends string> = {
   [state in States]?: {
@@ -35,16 +36,20 @@ export class StateMachine<States extends string, Actions extends string> {
 export const useStateMachine = <States extends string, Actions extends string>(
   stateMachine: StateMachine<States, Actions>,
 ) => {
+  // Machine state with only State type
   const [currentState, setCurrentState] = useState<States>(stateMachine.getInitialState);
 
+  // Check if machine can do provided action depends on predefined transitions
   const transition = (state: States, action: Actions): States => {
     const nextState = stateMachine.getTransitions?.[state]?.[action] as States;
 
     return nextState || state;
   };
 
+  // Update machine state besed on transition function above
   const updateState = (action: Actions) => setCurrentState(state => transition(state, action));
 
+  // Compare provided state to current one
   const compareState = (state: States) => currentState === state;
 
   return {
