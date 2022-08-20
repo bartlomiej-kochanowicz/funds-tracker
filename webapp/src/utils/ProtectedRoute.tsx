83ed68bbd669f47a82189ed772854e1c0c4c2de8
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-20 23:50:56 +0200

    reverse protected route

--- webapp/src/utils/ProtectedRoute.tsx
+++ webapp/src/utils/ProtectedRoute.tsx
@@ -1,14 +1,28 @@
-import { useSelector } from 'react-redux';
+import { getAccessToken } from 'helpers/accessTokens';
 import { Navigate } from 'react-router-dom';
 import { ROUTES } from 'routes';
-import { selectSigninData } from 'store/selectors/auth';
 
 interface ProtectedRouteProps {
   children: JSX.Element;
+  to?: string;
+  reverse?: boolean;
 }
 
-export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const { accessToken } = useSelector(selectSigninData);
+export const ProtectedRoute = ({
+  children,
+  to = ROUTES.SIGNIN,
+  reverse = false,
+}: ProtectedRouteProps) => {
+  const accessToken = getAccessToken();
 
-  return accessToken ? children : <Navigate to={ROUTES.SIGNIN} />;
+  if (reverse) return accessToken ? <Navigate to={to} /> : children;
+
+  return accessToken ? children : <Navigate to={to} />;
+};
+
+ProtectedRoute.displayName = 'ProtectedRoute';
+
+ProtectedRoute.defaultProps = {
+  to: ROUTES.SIGNIN,
+  reverse: false,
 };
