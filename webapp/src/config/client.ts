40b86605f2625b623d56c75f9273b0435ee45ba7
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-21 18:36:27 +0200

    use logout action

--- webapp/src/config/client.ts
+++ webapp/src/config/client.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { API_URL } from 'config/env';
+import { getLocalAuth } from 'helpers/userAuth';
+
+const token = getLocalAuth()?.accessToken;
 
 export const client = axios.create({
   baseURL: API_URL,
   timeout: 5000,
+  headers: { Authorization: `Bearer ${token}` },
 });
