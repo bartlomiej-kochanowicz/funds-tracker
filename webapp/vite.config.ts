99ee6c9621b7adadf51a813908597d71402971fd
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-08-30 23:36:47 +0200

    change port

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import analyze from 'rollup-plugin-analyzer';
 import svgr from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
@@ -10,14 +9,13 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 3000,
   },
   plugins: [
     react(),
     tsconfigPaths(),
     svgr(),
     eslint(),
-    analyze(),
     viteStaticCopy({
       targets: [
         {
