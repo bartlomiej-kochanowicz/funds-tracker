ec879d3b45e7dea5a7c6c222dae515ec4b3ed5c6
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-09-23 22:55:40 +0200

    test input email

--- webapp/src/views/Signin/Signin.po.tsx
+++ webapp/src/views/Signin/Signin.po.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { getByTestId, render } from 'utils/test-utils';
+import { fireEvent, getByTestId, render } from 'utils/test-utils';
 import { unsafeCast } from 'utils/unsafeCast';
 
 export class SigninPO {
   private elements: {
     chooseEmailInput: HTMLElement;
+    chooseSubmitButton: HTMLElement;
   };
 
   private elementsByDataID(container: HTMLElement) {
@@ -13,11 +14,22 @@ export class SigninPO {
       get chooseEmailInput() {
         return getByTestId(container, 'email-input');
       },
+      get chooseSubmitButton() {
+        return getByTestId(container, 'submit-button');
+      },
     };
   }
 
   protected constructor(protected container: HTMLElement) {
     this.elements = this.elementsByDataID(container);
+  }
+
+  setEmail(value: string) {
+    fireEvent.change(this.elements.chooseEmailInput, { target: { value } });
+  }
+
+  expectButtonHasProperText(text: string) {
+    expect(this.elements.chooseSubmitButton).toHaveTextContent(text);
   }
 
   static render(SigninComponent: FC) {
