71abd1bc0fc76f532643ec419fa99b884f5d1d99
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-09-23 22:39:11 +0200

    fix translations

--- webapp/src/utils/i18nForTests.ts
+++ webapp/src/utils/i18nForTests.ts
@@ -3,12 +3,13 @@ import { initReactI18next } from 'react-i18next';
 import en from 'locales/en.json';
 
 i18next.use(initReactI18next).init({
+  lng: 'en',
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },
   load: 'languageOnly',
-  resources: { en },
+  resources: { en: { translation: en } },
 });
 
 export default i18next;
