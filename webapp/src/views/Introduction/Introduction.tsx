775a463158e314d07ccec7fad5a1489f80f891a3
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-09-25 16:05:48 +0200

    add components with view

--- webapp/src/views/Introduction/Introduction.tsx
+++ webapp/src/views/Introduction/Introduction.tsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from 'react';
 import { FullscreenClear } from 'layouts/FullscreenClear';
 import { IntroductionProvider, useIntroductionContext } from './context';
+import { Welcome } from './components/Welcome';
+import { AddCashAccountsForm } from './components/AddCashAccountsForm';
 
 const AddInstrumentForm = lazy(() =>
   import('./components/AddInstrumentForm').then(({ AddInstrumentForm: component }) => ({
@@ -20,6 +22,10 @@ const IntroductionContent = () => {
   return (
     <FullscreenClear>
       <Suspense>
+        {compareState(states.welcome) && <Welcome />}
+
+        {compareState(states.addCashAccounts) && <AddCashAccountsForm />}
+
         {compareState(states.addInstrument) && <AddInstrumentForm />}
 
         {compareState(states.formSuccess) && <FormSuccess />}
