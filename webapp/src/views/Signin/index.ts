6d74c535fc8c59722bc622ba85f7f2e011454ee0
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-10-05 22:15:35 +0200

    signin refactor

--- webapp/src/views/Signin/index.ts
+++ webapp/src/views/Signin/index.ts
@@ -1,1 +1,1 @@
-export * from './Signin';
+export * from './routes';
---
+++ webapp/src/views/Signin/routes/Routes.tsx
@@ -0,0 +1,21 @@
+import { lazy } from 'react';
+import { ROUTES } from 'routes';
+import { ProtectedRoute } from 'utils/ProtectedRoute';
+
+const Signin = lazy(() =>
+  import('./Signin').then(({ Signin: component }) => ({ default: component })),
+);
+
+export const SinginRoutes = [
+  {
+    path: ROUTES.SIGNIN,
+    element: (
+      <ProtectedRoute
+        to={ROUTES.DASHBOARD.HOME}
+        reverse
+      >
+        <Signin />
+      </ProtectedRoute>
+    ),
+  },
+];
