5c38322a8f6a8105f7581e100e21b702df8d4edc
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-10-27 21:14:18 +0200

    send token in check email and signin requests

--- webapp/src/services/auth/signin.ts
+++ webapp/src/services/auth/signin.ts
@@ -4,13 +4,15 @@ import { retryHTTP } from 'utils/retryHTTP';
 export interface SigninProps {
   userEmail: string;
   userPassword: string;
+  token: string;
 }
 
 export const signin = retryHTTP(
-  ({ userEmail, userPassword }: SigninProps) =>
+  ({ userEmail, userPassword, token }: SigninProps) =>
     clientPrivate.post('/auth/local/signin', {
       email: userEmail,
       password: userPassword,
+      token,
     }),
   {
     maxAttempts: 3,
