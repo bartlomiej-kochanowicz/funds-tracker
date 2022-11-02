81f37ff236ae703256498fb266b65bae7288a5e6
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-11-02 21:34:10 +0100

    new favicon & optimise fonts loading

--- webapp/src/styles/GlobalStyle.ts
+++ webapp/src/styles/GlobalStyle.ts
@@ -11,7 +11,7 @@ export const GlobalStyle = styled.createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    font-family: 'Roboto', sans-serif;
+    font-family: Roboto, sans-serif;
   }
 
   * {
