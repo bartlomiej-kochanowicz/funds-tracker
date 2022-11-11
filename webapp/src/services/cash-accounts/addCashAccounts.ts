6f291846d020e575c9706bdd2cd08f55bde8bcb7
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-11-11 17:28:19 +0100

    add cash account service

---
+++ webapp/src/services/cash-accounts/addCashAccounts.ts
@@ -0,0 +1,24 @@
+import { clientPrivate } from 'config/privateClient';
+import { Currencies } from 'types/currencies';
+import { retryHTTP } from 'utils/retryHTTP';
+
+export type CashAccount = {
+  uuid?: string;
+  name: string;
+  currency: Currencies;
+  balance?: number;
+};
+
+export interface AddCashAccountsProps {
+  cashAccounts: CashAccount[];
+}
+
+export const addCashAccounts = retryHTTP(
+  ({ cashAccounts }: AddCashAccountsProps) =>
+    clientPrivate.post('/cash-accounts', {
+      cashAccounts,
+    }),
+  {
+    maxAttempts: 3,
+  },
+);
