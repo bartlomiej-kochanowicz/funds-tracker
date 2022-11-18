cb1e301132d016975f75e29ddf77dfd405838294
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-11-18 17:30:35 +0100

    config tslint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['./tsconfig.json', './api/tsconfig.json'],
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
