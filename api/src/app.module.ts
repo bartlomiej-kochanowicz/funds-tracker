e4fc9b5eaac0504ef4a2c5e85bd250b26b908a96
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-11-25 18:19:22 +0100

    configure graphql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,10 @@
+import { join } from 'path';
 import { APP_GUARD } from '@nestjs/core';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AtGuard } from 'common/guards';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
@@ -12,6 +16,15 @@ import { PortfoliosModule } from './portfolios/portfolios.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
     }),
     PrismaModule,
     AuthModule,
