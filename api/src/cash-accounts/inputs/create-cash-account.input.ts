ffb939447129a20090108f6ad55c426ed43140f0
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-11-26 16:55:58 +0100

    add class-validator

--- api/src/cash-accounts/inputs/create-cash-account.input.ts
+++ api/src/cash-accounts/inputs/create-cash-account.input.ts
@@ -1,8 +1,16 @@
 import { InputType, Field, registerEnumType } from '@nestjs/graphql';
 import { Currency } from '@prisma/client';
+import { MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateCashAccountInput {
+  @MinLength(2, {
+    message: 'Name is short',
+    context: 'name',
+  })
+  @MaxLength(50, {
+    message: 'Name is too long',
+  })
   @Field(() => String, { description: 'Cash account name.' })
   name: string;
 
--- api/src/portfolios/inputs/create-portfolio.input.ts
+++ api/src/portfolios/inputs/create-portfolio.input.ts
@@ -1,7 +1,15 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class CreatePortfolioInput {
+  @MinLength(2, {
+    message: 'Name is short',
+    context: 'name',
+  })
+  @MaxLength(50, {
+    message: 'Name is too long',
+  })
   @Field(() => String, { description: 'Portfolio name.' })
   name: string;
 
