8e0960c447b6ede10b1dd3f5b8e49d363f3b6e2d
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-11-26 15:15:06 +0100

    update portfolios service

--- api/src/portfolios/portfolios.service.ts
+++ api/src/portfolios/portfolios.service.ts
@@ -1,8 +1,9 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { MAX_PORTFOLIOS } from 'common/constants/common';
 import { PrismaService } from 'prisma/prisma.service';
-import { CreatePortfolioDto } from './dto/create-portfolio.dto';
-import { UpdatePortfolioDto } from './dto/update-portfolio.dto';
+import { Portfolio } from './entities/portfolio.entity';
+import { CreatePortfolioInput } from './inputs/create-portfolio.input';
+import { UpdatePortfolioInput } from './inputs/update-portfolio.input';
 
 @Injectable()
 export class PortfoliosService {
@@ -10,21 +11,21 @@ export class PortfoliosService {
 
   async create(
     userUuid: string,
-    createPortfolioDto: CreatePortfolioDto,
-  ): Promise<null> {
+    createPortfolioInput: CreatePortfolioInput,
+  ): Promise<Portfolio> {
     const portfolios = await this.prisma.portfolio.count({
       where: {
         userUuid,
       },
     });
 
-    const { name, rebalancingEnabled } = createPortfolioDto;
+    const { name, rebalancingEnabled } = createPortfolioInput;
 
     if (portfolios > MAX_PORTFOLIOS) {
       throw new HttpException('Max portfolios reached', HttpStatus.FORBIDDEN);
     }
 
-    await this.prisma.portfolio.create({
+    const portfolio = await this.prisma.portfolio.create({
       data: {
         name,
         rebalancingEnabled,
@@ -32,10 +33,10 @@ export class PortfoliosService {
       },
     });
 
-    return null;
+    return portfolio;
   }
 
-  async findAll(userUuid: string): Promise<CreatePortfolioDto[]> {
+  async findAll(userUuid: string): Promise<Portfolio[]> {
     const portfolios = await this.prisma.portfolio.findMany({
       where: {
         userUuid,
@@ -50,7 +51,7 @@ export class PortfoliosService {
     return portfolios;
   }
 
-  async findOne(userUuid: string, uuid: string): Promise<CreatePortfolioDto> {
+  async findOne(userUuid: string, uuid: string): Promise<Portfolio> {
     const portfolio = await this.prisma.portfolio.findUnique({
       where: {
         userUuid_uuid: {
@@ -71,28 +72,28 @@ export class PortfoliosService {
   async update(
     userUuid: string,
     uuid: string,
-    updatePortfolioDto: UpdatePortfolioDto,
-  ): Promise<null> {
+    updatePortfolioInput: UpdatePortfolioInput,
+  ): Promise<Portfolio> {
     try {
-      await this.prisma.portfolio.update({
+      const portfolio = await this.prisma.portfolio.update({
         where: {
           userUuid_uuid: {
             userUuid,
             uuid,
           },
         },
-        data: updatePortfolioDto,
+        data: updatePortfolioInput,
       });
 
-      return null;
+      return portfolio;
     } catch {
       throw new HttpException('Portfolio not found', HttpStatus.NOT_FOUND);
     }
   }
 
-  async remove(userUuid: string, uuid: string): Promise<null> {
+  async remove(userUuid: string, uuid: string): Promise<Portfolio> {
     try {
-      await this.prisma.portfolio.delete({
+      const portfolio = await this.prisma.portfolio.delete({
         where: {
           userUuid_uuid: {
             userUuid,
@@ -101,7 +102,7 @@ export class PortfoliosService {
         },
       });
 
-      return null;
+      return portfolio;
     } catch {
       throw new HttpException('Account not found', HttpStatus.NOT_FOUND);
     }
