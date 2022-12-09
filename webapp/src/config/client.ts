91cf92b1bc7a509b072b472c45951bc228927b1c
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-12-09 13:35:33 +0100

    retry http

--- webapp/src/config/client.ts
+++ webapp/src/config/client.ts
@@ -1,10 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { RetryLink } from '@apollo/client/link/retry';
 import { API_URL, IS_DEVELOPMENT } from 'config/env';
 
 const client = new ApolloClient({
-  uri: IS_DEVELOPMENT ? API_URL : '/api/graphql',
   cache: new InMemoryCache(),
   credentials: 'include',
+  link: new HttpLink({
+    uri: IS_DEVELOPMENT ? API_URL : '/api/graphql',
+  }).concat(
+    new RetryLink({
+      delay: {
+        initial: 300,
+        max: Infinity,
+        jitter: true,
+      },
+      attempts: {
+        max: 3,
+        retryIf: error => Boolean(error),
+      },
+    }),
+  ),
 });
 
 export default client;
