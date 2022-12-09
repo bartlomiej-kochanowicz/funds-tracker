c5457dc2199d362e87ac1401ee1771535a9a76f9
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2022-12-09 13:25:57 +0100

    remove retryhttp

--- webapp/src/helpers/localAccount.ts
+++
@@ -1,8 +0,0 @@
-import { AccountResponse } from 'services/auth/account';
-import { saveStore, loadStore, removeStore } from 'utils/localStorage';
-import { isUserLoggedIn } from 'helpers/isUserLoggedIn';
-
-export const setLocalAccount = (auth: AccountResponse) => saveStore(auth, 'account');
-export const removeLocalAccount = () => removeStore('account');
-export const getLocalAccount = (): AccountResponse | undefined =>
-  isUserLoggedIn ? loadStore('account') : removeLocalAccount();
