ade6ffb45178766f2c9675bb18307e9e053806ab
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-01-12 23:27:39 +0100

    add margin on mobile

--- webapp/src/layouts/Dashboard/Dashboard.styles.ts
+++ webapp/src/layouts/Dashboard/Dashboard.styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100%;
-  height: var(--doc-height);
+  min-height: 100%;
+  min-height: var(--doc-height);
 
   ${({ theme }) => css`
     background-color: ${theme.colors.gray100};
@@ -12,6 +12,10 @@ export const Wrapper = styled.div`
       margin-left: 270px;
       padding: ${theme.spacing.huge};
       padding-top: 98px;
+    }
+
+    ${theme.breakpoints.desktop.max} {
+      padding-bottom: 150px;
     }
   `}
 `;
