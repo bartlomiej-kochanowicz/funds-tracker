6389929bf531b01ef581e031dda8fe7e12be2456
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-01-21 14:47:05 +0100

    modal title

--- webapp/src/components/molecules/Modal/Modal.tsx
+++ webapp/src/components/molecules/Modal/Modal.tsx
@@ -2,17 +2,26 @@ import { FC, Fragment, useRef } from 'react';
 import { Row } from 'simple-flexbox';
 import { FaTimes } from 'react-icons/fa';
 import { modals, ModalsNames } from 'modals';
-import { Spacer } from 'components/atoms';
+import { Heading, Spacer, Spreader } from 'components/atoms';
 import { useDetectOutsideClick } from 'hooks/useDetectOutsideClick';
+import { useTranslation } from 'react-i18next';
 import { Modal } from './Modal.styles';
 
 interface ModalComponentProps {
   name: ModalsNames;
   closeModal: () => void;
+  showName?: boolean;
 }
 
-export const ModalComponent: FC<ModalComponentProps> = ({ name, closeModal, ...rest }) => {
+export const ModalComponent: FC<ModalComponentProps> = ({
+  name,
+  closeModal,
+  showName = true,
+  ...rest
+}) => {
   const modalRef = useRef<HTMLDivElement>(null);
+
+  const { t } = useTranslation();
 
   const Component = modals[name];
 
@@ -24,6 +33,16 @@ export const ModalComponent: FC<ModalComponentProps> = ({ name, closeModal, ...r
 
       <Modal ref={modalRef}>
         <Row justifyContent="flex-end">
+          {showName && (
+            <Fragment>
+              <Heading level="h2">{t(`modal.name.${name}`)}</Heading>
+
+              <Spreader spread="huge" />
+
+              <Spreader spread="huge" />
+            </Fragment>
+          )}
+
           <Modal.CloseButton onClick={closeModal}>
             <FaTimes size="1.25rem" />
           </Modal.CloseButton>
--- webapp/src/modals/CreateCashAccount/CreateCashAccount.tsx
+++ webapp/src/modals/CreateCashAccount/CreateCashAccount.tsx
@@ -5,13 +5,15 @@ export const MODAL_CREATE_CASH_ACCOUNT = 'CreateCashAccount';
 
 export type CreateCashAccountModalProps = {};
 
-export const CreateCashAccount: FC<Modal<CreateCashAccountModalProps>> = ({ closeModal }) => (
-  <div>
-    <button
-      type="button"
-      onClick={closeModal}
-    >
-      CreateCashAccount
-    </button>
-  </div>
-);
+export const CreateCashAccount: FC<Modal<CreateCashAccountModalProps>> = ({ closeModal }) => {
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={closeModal}
+      >
+        CreateCashAccount
+      </button>
+    </div>
+  );
+};
