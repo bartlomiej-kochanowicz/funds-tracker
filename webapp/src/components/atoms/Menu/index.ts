b55dab8cc3e86253b1bfa434d62db4cf555ca80f
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-02-01 00:21:51 +0100

    fix types in menu

--- webapp/src/components/atoms/Menu/index.ts
+++ webapp/src/components/atoms/Menu/index.ts
@@ -1,17 +1,23 @@
 import { FC } from 'react';
-import { DefaultTheme, StyledComponent } from 'styled-components';
+import { DefaultTheme, StyledComponent, StyledComponentBase } from 'styled-components';
+import { ForwardRefComponent, HTMLMotionProps } from 'framer-motion';
 import { Menu as MenuComponent, MenuProps } from './Menu';
 import { MenuItem, MenuItemProps } from './MenuItem';
 import { MenuDivider, MenuDividerProps } from './MenuDivider';
 
 interface IMenu
-  extends React.ForwardRefExoticComponent<MenuProps & React.RefAttributes<HTMLUListElement>> {
+  extends StyledComponentBase<
+    ForwardRefComponent<HTMLUListElement, HTMLMotionProps<'ul'>>,
+    DefaultTheme,
+    MenuProps,
+    never
+  > {
   Item: FC<MenuItemProps>;
   Divider: StyledComponent<'hr', DefaultTheme, MenuDividerProps, never>;
 }
 
 export const Menu = {
-  ...MenuComponent,
+  ...(MenuComponent as any),
   Item: MenuItem,
   Divider: MenuDivider,
 } as IMenu;
