40468a6958e1392e307cc1dc96bbde34c7e5c3ce
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-02-03 21:42:46 +0100

    add form

--- webapp/src/views/ResetPassword/routes/ResetPassword/components/EnterPassword/EnterPasswordForm.tsx
+++ webapp/src/views/ResetPassword/routes/ResetPassword/components/EnterPassword/EnterPasswordForm.tsx
@@ -1,11 +1,100 @@
-import { FC } from 'react';
+import { Button, Input, Loader, Spacer } from 'components/atoms';
+import { FC, lazy, Suspense, useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useInput } from 'hooks/useInput';
+import { Form } from './EnterPassword.styles';
+import { validationSchema } from './EnterPassword.schema';
+
+const GoogleReCaptcha = lazy(() =>
+  import('react-google-recaptcha-v3').then(({ GoogleReCaptcha: component }) => ({
+    default: component,
+  })),
+);
 
 interface EnterPasswordFormProps {
   token: string;
 }
 
-export const EnterPasswordForm: FC<EnterPasswordFormProps> = ({ token }) => {
-  return <div>form</div>;
+export const EnterPasswordForm: FC<EnterPasswordFormProps> = ({ token: resetPasswordToken }) => {
+  const { t } = useTranslation();
+
+  const [token, setToken] = useState<string>('');
+  const [refreshReCaptcha, setRefreshReCaptcha] = useState<boolean>(false);
+
+  const onVerify = useCallback(setToken, [setToken]);
+
+  const defaultValues = {
+    userPassword: '',
+    userPasswordConfirmation: '',
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues,
+    resolver: yupResolver(validationSchema),
+  });
+
+  const userPasswordProps = useInput<typeof defaultValues>({
+    register,
+    name: 'userPassword',
+    errors,
+  });
+
+  const userPasswordConfirmationProps = useInput<typeof defaultValues>({
+    register,
+    name: 'userPasswordConfirmation',
+    errors,
+  });
+
+  const onSubmit = async ({ userPassword }: typeof defaultValues) => {
+    setRefreshReCaptcha(r => !r);
+  };
+
+  return (
+    <Form
+      onSubmit={handleSubmit(onSubmit)}
+      noValidate
+    >
+      <Suspense>
+        <GoogleReCaptcha
+          onVerify={onVerify}
+          refreshReCaptcha={refreshReCaptcha}
+        />
+      </Suspense>
+
+      <Input
+        placeholder={t('common.password')}
+        type="password"
+        autoFocus
+        {...userPasswordProps}
+      />
+
+      <Spacer />
+
+      <Input
+        placeholder={t('page.signup.password.confirm')}
+        type="password"
+        {...userPasswordConfirmationProps}
+      />
+
+      <Spacer />
+
+      <Button
+        width="auto"
+        disabled={isSubmitting}
+        type="submit"
+      >
+        {isSubmitting && <Loader color="white" />}
+
+        {!isSubmitting && t('common.save')}
+      </Button>
+    </Form>
+  );
 };
 
 EnterPasswordForm.displayName = 'EnterPasswordForm';
