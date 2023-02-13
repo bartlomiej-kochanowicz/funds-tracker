1e585fa393fdca7d48b33ef398e7b3d654894768
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-02-13 22:54:36 +0100

    rename cash account

--- webapp/src/modals/RenameCashAccount/RenameCashAccount.tsx
+++ webapp/src/modals/RenameCashAccount/RenameCashAccount.tsx
@@ -1,10 +1,17 @@
+import { useMutation } from '@apollo/client';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Button, Input, Loader, Spacer, Spreader } from 'components/atoms';
+import { UPDATE_CASH_ACCOUNT } from 'graphql/mutations';
+import { showErrorToast } from 'helpers/showToast';
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { Row } from 'simple-flexbox';
 import { Modal } from 'types/modal.type';
+import {
+  UpdateCashAccountMutation,
+  UpdateCashAccountMutationVariables,
+} from '__generated__/graphql';
 import { validationSchema } from './RenameCashAccount.schema';
 
 export const MODAL_RENAME_CASH_ACCOUNT_MODAL = 'RenameCashAccount';
@@ -33,8 +40,28 @@ export const RenameCashAccount: FC<Modal<RenameCashAccountProps>> = ({
     register,
   } = useForm({ defaultValues, resolver: yupResolver(validationSchema), mode: 'onChange' });
 
+  const [updateCashAccount] = useMutation<
+    UpdateCashAccountMutation,
+    UpdateCashAccountMutationVariables
+  >(UPDATE_CASH_ACCOUNT, {
+    onCompleted: ({ updateCashAccount: { name: newName } }) => {
+      callback({ name: newName, uuid });
+
+      closeModal();
+    },
+    onError: () => {
+      closeModal();
+
+      showErrorToast(t('service.unknown_error'));
+    },
+  });
+
+  const onSubmit = async ({ name: newName }: typeof defaultValues) => {
+    await updateCashAccount({ variables: { data: { name: newName }, uuid } });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Input
         defaultValue={defaultValues.name}
         placeholder={t('common.input.name.placeholder')}
@@ -62,7 +89,7 @@ export const RenameCashAccount: FC<Modal<RenameCashAccountProps>> = ({
         >
           {isSubmitting && <Loader color="white" />}
 
-          {!isSubmitting && t('page.cash_accounts.button.add_funds')}
+          {!isSubmitting && t('common.save')}
         </Button>
       </Row>
     </form>
