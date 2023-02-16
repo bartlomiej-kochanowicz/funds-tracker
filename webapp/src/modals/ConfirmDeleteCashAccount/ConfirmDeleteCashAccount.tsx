b60145c72f1ee838260b9205b7b638035a7274f5
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-02-16 08:37:28 +0100

    add delete styles

--- webapp/src/modals/ConfirmDeleteCashAccount/ConfirmDeleteCashAccount.tsx
+++ webapp/src/modals/ConfirmDeleteCashAccount/ConfirmDeleteCashAccount.tsx
@@ -1,4 +1,7 @@
+import { Button, Spacer, Spreader, Text } from 'components/atoms';
 import { FC } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { Column, Row } from 'simple-flexbox';
 import { Modal } from 'types/modal.type';
 
 export const MODAL_CONFIRM_DELETE_CASH_ACCOUNT = 'ConfirmDeleteCashAccount';
@@ -7,6 +10,50 @@ export interface ConfirmDeleteCashAccountProps {
   name: string;
 }
 
-export const ConfirmDeleteCashAccount: FC<Modal<ConfirmDeleteCashAccountProps>> = ({ name }) => {
-  return <div>ConfirmDeleteCashAccount - {name}</div>;
+export const ConfirmDeleteCashAccount: FC<Modal<ConfirmDeleteCashAccountProps>> = ({
+  name,
+  closeModal,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Column>
+      <Text
+        fontSize="0.875"
+        fontColor="gray400"
+        textAlign="center"
+      >
+        <Trans
+          i18nKey="modal.ConfirmDeleteCashAccount.description"
+          values={{ name }}
+          components={{
+            bold: (
+              <Text
+                fontSize="0.875"
+                fontColor="gray400"
+                textAlign="center"
+                fontWeight="700"
+              />
+            ),
+          }}
+        />
+      </Text>
+
+      <Spacer />
+
+      <Row>
+        <Button
+          color="tertiary"
+          onClick={closeModal}
+          width="50%"
+        >
+          {t('common.no')}
+        </Button>
+
+        <Spreader spread="small" />
+
+        <Button width="50%">{t('common.yes')}</Button>
+      </Row>
+    </Column>
+  );
 };
