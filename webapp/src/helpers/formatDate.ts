700f9711563d8cf1c16db2725de80587cd1e2aa8
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-02-25 00:05:37 +0100

    date format

---
+++ webapp/src/helpers/formatDate.ts
@@ -0,0 +1,14 @@
+import i18n from 'utils/i18n';
+
+export const formatDate = (date: string): string => {
+  const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
+
+  return new Date(date).toLocaleDateString(i18n.language, {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone,
+  });
+};
--- webapp/src/modals/ManageCashAccount/components/CashAccountsOperations/CashAccountOperations.tsx
+++ webapp/src/modals/ManageCashAccount/components/CashAccountsOperations/CashAccountOperations.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { Loader } from 'components/atoms';
 import { GET_CASH_ACCOUNT_OPERATIONS } from 'graphql/query/GetCashAccountOperations';
+import { formatDate } from 'helpers/formatDate';
 import { FC } from 'react';
 import {
   GetCashAccountOperationsQuery,
@@ -12,7 +13,7 @@ interface CashAccountOperationsProps {
 }
 
 export const CashAccountOperations: FC<CashAccountOperationsProps> = ({ uuid }) => {
-  const { loading } = useQuery<
+  const { loading, data } = useQuery<
     GetCashAccountOperationsQuery,
     GetCashAccountOperationsQueryVariables
   >(GET_CASH_ACCOUNT_OPERATIONS, { variables: { uuid } });
@@ -21,5 +22,19 @@ export const CashAccountOperations: FC<CashAccountOperationsProps> = ({ uuid }) 
     return <Loader />;
   }
 
-  return null;
+  if (!loading && !data?.cashAccount.operations.length) {
+    return <div>There are no operations for this cash account.</div>;
+  }
+
+  return (
+    <div>
+      {data?.cashAccount.operations.map(operation => {
+        return (
+          <div key={operation.uuid}>
+            {operation.type} {operation.amount} {formatDate(operation.date)}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
