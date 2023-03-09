24573a4bc9ab3f2b66625f7507e10578ec80fa9f
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-03-09 17:20:52 +0100

    move subscriptions to webserver

--- webserver/src/common/constants/subscription.ts
+++ webserver/src/common/constants/subscription.ts
@@ -1,19 +1,24 @@
 type Limits = {
   maxCashAccounts: number;
+  maxPortfolios: number;
 };
 
 export default {
   FREE: {
     maxCashAccounts: 2,
+    maxPortfolios: 1,
   },
   LITE: {
     maxCashAccounts: 10,
+    maxPortfolios: 5,
   },
   PROFESSIONAL: {
     maxCashAccounts: 25,
+    maxPortfolios: 15,
   },
   EXPERT: {
     maxCashAccounts: 100,
+    maxPortfolios: 100,
   },
 } satisfies {
   FREE: Limits;
