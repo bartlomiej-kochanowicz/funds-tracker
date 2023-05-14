deb92f858a96ebd02e16796e94468907373784e0
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-05-15 00:32:22 +0200

    portfolio menu

--- webapp/src/views/Authenticated/Portfolios/routes/Portfolios/components/PortfolioPanel/PortfolioPanel.tsx
+++ webapp/src/views/Authenticated/Portfolios/routes/Portfolios/components/PortfolioPanel/PortfolioPanel.tsx
@@ -1,13 +1,18 @@
 import { GetPortfoliosQuery } from '__generated__/graphql';
+import NiceModal from '@ebay/nice-modal-react';
 import { Box, Button, Dropdown, Icon, Text } from 'components/atoms';
+import { DropdownItems } from 'components/atoms/Dropdown';
 import { useUserContext } from 'contexts/UserContext';
 import { formatCurrency } from 'helpers/formatCurrency';
 import { useBreakpoint } from 'hooks/useBreakpoint';
+import { MODAL_CONFIRM_DELETE_PORTFOLIO } from 'modals/ConfirmDeletePortfolio';
+import { MODAL_RENAME_PORTFOLIO } from 'modals/RenamePortfolio';
 import { FC } from 'react';
-import { FaEllipsisV } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
+import { FaChartPie, FaEllipsisV, FaPen, FaTrash } from 'react-icons/fa';
+import { generatePath } from 'react-router-dom';
+import { ROUTES } from 'routes/paths';
 import { Column, Row } from 'simple-flexbox';
-
-import { DropdownContent } from './components/DropdownContent';
 
 interface PortfoliosPanelProps {
   updatePortfolioName: (data: { name: string; uuid: string }) => void;
@@ -24,17 +29,47 @@ export const PortfolioPanel: FC<GetPortfoliosQuery['portfolios'][0] & Portfolios
     user: { defaultCurrency },
   } = useUserContext();
 
+  const { t } = useTranslation();
+
   const isPhone = useBreakpoint('phone', 'max');
 
-  /* const content = (props: ContentProps) => (
-    <DropdownContent
-      uuid={uuid}
-      name={name}
-      updatePortfolioName={updatePortfolioName}
-      updatePortfolioList={updatePortfolioList}
-      {...props}
-    />
-  ); */
+  const handleOpenRenameModal = () => {
+    NiceModal.show(MODAL_RENAME_PORTFOLIO, {
+      uuid,
+      name,
+      callback: updatePortfolioName,
+    });
+  };
+
+  const handleOpenDeletePortfolioModal = () => {
+    NiceModal.show(MODAL_CONFIRM_DELETE_PORTFOLIO, {
+      name,
+      uuid,
+      callback: updatePortfolioList,
+    });
+  };
+
+  const items = [
+    {
+      value: 'manage',
+      label: t('common.manage'),
+      to: generatePath(ROUTES.PORTFOLIOS.PORTFOLIO, { uuid }),
+      divider: 'bottom',
+      icon: FaChartPie,
+    },
+    {
+      value: 'rename',
+      label: t('common.rename'),
+      onClick: handleOpenRenameModal,
+      icon: FaPen,
+    },
+    {
+      value: 'delete',
+      label: t('common.delete'),
+      onClick: handleOpenDeletePortfolioModal,
+      icon: FaTrash,
+    },
+  ] satisfies DropdownItems;
 
   return (
     <Box
@@ -48,8 +83,8 @@ export const PortfolioPanel: FC<GetPortfoliosQuery['portfolios'][0] & Portfolios
           <Text>{formatCurrency(2137, defaultCurrency)}</Text>
         </Column>
 
-        {/* <Dropdown
-          content={content}
+        <Dropdown
+          items={items}
           placement="bottom-end"
         >
           {props => (
@@ -61,7 +96,7 @@ export const PortfolioPanel: FC<GetPortfoliosQuery['portfolios'][0] & Portfolios
               <Icon icon={FaEllipsisV} />
             </Button>
           )}
-        </Dropdown> */}
+        </Dropdown>
       </Row>
     </Box>
   );
