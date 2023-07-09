4de02b49ff6bf19bc19cc33483a4c81a6300606d
bartlomiej-kochanowicz <bkochanovitz@gmail.com>
2023-07-09 20:38:10 +0200

    please select instrument type

--- webapp/src/modals/InvestFunds/components/PleaseSelectInstrumentType/PleaseSelectInstrumentType.tsx
+++ webapp/src/modals/InvestFunds/components/PleaseSelectInstrumentType/PleaseSelectInstrumentType.tsx
@@ -1,3 +1,23 @@
+import { Box, Heading, Spacer } from 'components/atoms';
+import { useTranslation } from 'react-i18next';
+
 export const PleaseSelectInstrumentType = () => {
-  return <div>Please select instrument type to continue.</div>;
+  const { t } = useTranslation();
+
+  return (
+    <Box
+      flex
+      flexDirection="column"
+    >
+      <Spacer space="0.5" />
+
+      <Heading
+        level="h2"
+        fontColor="gray300"
+        textAlign="center"
+      >
+        {t('modal.InvestFunds.form.please.select.instrumentType.to.continue')}
+      </Heading>
+    </Box>
+  );
 };
