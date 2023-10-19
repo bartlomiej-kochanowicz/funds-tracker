918b45659e937d7c088e3914d619437b458de3d3
Bartek Kochanowicz <bkochanovitz@gmail.com>
2023-10-19 20:03:48 +0200

    final nest config

--- packages/eslint-config-custom/typescript.js
+++ packages/eslint-config-custom/typescript.js
@@ -1,29 +1,17 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
 module.exports = {
 	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		project,
-		sourceType: "module",
-	},
-	plugins: ["@typescript-eslint", "eslint-plugin-prettier", "prettier"],
-	extends: [
-		"airbnb-base",
-		"airbnb-typescript/base",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:prettier/recommended",
-		"prettier",
-	],
-	settings: {
-		"import/resolver": {
-			typescript: {},
-		},
-	},
+	plugins: ["@typescript-eslint/eslint-plugin"],
+	extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
 	root: true,
 	env: {
 		node: true,
 		jest: true,
+	},
+	ignorePatterns: [".eslintrc.js"],
+	rules: {
+		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-explicit-any": "off",
 	},
 };
