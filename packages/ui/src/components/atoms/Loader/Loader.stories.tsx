29823ac7bc4338fd29f241d6f70392a432854771
Bartek Kochanowicz <bkochanovitz@gmail.com>
2023-11-17 12:49:43 +0100

    loader

---
+++ packages/ui/src/components/atoms/Loader/Loader.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Loader } from "./Loader";
+
+const meta: Meta<typeof Loader> = {
+	component: Loader,
+};
+
+export default meta;
+type Story = StoryObj<typeof Loader>;
+
+export const Default: Story = {
+	render: () => <Loader />,
+};
