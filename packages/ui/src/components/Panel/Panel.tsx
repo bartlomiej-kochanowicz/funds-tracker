97c4e940c2db2c5f737b25ab7d1b29f53cf5d945
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-01-05 15:49:22 +0100

    fix eslint

--- packages/ui/src/components/Panel/Panel.tsx
+++ packages/ui/src/components/Panel/Panel.tsx
@@ -9,7 +9,8 @@ interface PanelProps extends HTMLAttributes<HTMLDivElement> {
 export const Panel: FC<PanelProps> = ({ children, className }) => (
 	<div
 		className={clsx(
-			"md:py-3dark:bg-neutral-700 rounded-xl border-gray-300 bg-white px-3 py-1 shadow-sm md:px-6",
+			"rounded-xl border-gray-300 bg-white px-3 py-1 shadow-sm dark:bg-neutral-700",
+			"md:px-6 md:py-3",
 			className,
 		)}
 	>
