3be847cbbf11840784de439a0af120d2cf71a773
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-01-07 21:47:16 +0100

    case sensitive fix

--- apps/api/src/invest/invest.resolver.ts
+++ apps/api/src/invest/invest.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Mutation, Args } from "@nestjs/graphql";
 import { GetCurrentUserId } from "@app/common/decorators";
 import { InvestService } from "./invest.service";
 import { InvestInNewInstrument } from "./entities/investInNewInstrument.entity";
-import { InvestInNewInstrumentInput } from "./inputs/InvestInNewInstrument.input";
+import { InvestInNewInstrumentInput } from "./inputs/investInNewInstrument.input";
 
 @Resolver()
 export class InvestResolver {
