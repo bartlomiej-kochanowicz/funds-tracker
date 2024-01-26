e4e79ce35de90bfb1ba7f21064e60832624fcd54
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-01-26 12:07:37 +0100

    remove react aria

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,7 +3,6 @@ export * from "./components/Avatar";
 export * from "./components/Badge";
 export * from "./components/Button";
 export * from "./components/Card";
-export * from "./components/Checkbox";
 export * from "./components/Command";
 export * from "./components/Dialog";
 export * from "./components/DropdownMenu";
@@ -13,14 +12,11 @@ export * from "./components/Input";
 export * from "./components/Label";
 export * from "./components/Loader";
 export * from "./components/Popover";
-export * from "./components/Radio";
 export * from "./components/ScrollArea";
 export * from "./components/Skeleton";
-export * from "./components/Switch";
 export * from "./components/Text";
 export * from "./components/Tile";
 
 // Path: src/hooks
-export * from "./hooks/useDatepicker";
 export * from "./hooks/useTailwindBreakpoint";
 // shadcn-ui
