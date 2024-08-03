53cc2535a3cc47e3c380a77869deb75d5ce1cb95
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-08-03 14:52:17 +0200

    remove tooltip

--- apps/webapp/src/components/dialogs/CashAccounts/CashAccountInvestFundsDialog/components/SearchInstrumentField/SearchInstrumentField.tsx
+++ apps/webapp/src/components/dialogs/CashAccounts/CashAccountInvestFundsDialog/components/SearchInstrumentField/SearchInstrumentField.tsx
@@ -1,6 +1,5 @@
-import { Form, Text, Tooltip, useUpdateEffect } from "@funds-tracker/ui";
+import { Form, useUpdateEffect } from "@funds-tracker/ui";
 import { SearchInstrumentCombobox } from "components/SearchInstrumentCombobox";
-import { Info } from "lucide-react";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
@@ -24,24 +23,7 @@ export const SearchInstrumentField = () => {
 			name="instrument"
 			render={({ field }) => (
 				<Form.Item orientation="horizontal">
-					<Form.Label className="flex min-w-44">
-						{t("common.instrument")}
-
-						<Tooltip.Provider delayDuration={250}>
-							<Tooltip>
-								<Tooltip.Trigger asChild>
-									<Info className="ml-2 size-4" />
-								</Tooltip.Trigger>
-								<Tooltip.Content className="max-w-60">
-									<Text>
-										We support more than 60 exchanges all around the world. All US exchanges are
-										combined into one virtual exchange &quot;US&quot; which includes NYSE, NASDAQ,
-										NYSE ARCA, and OTC/PINK tickers.
-									</Text>
-								</Tooltip.Content>
-							</Tooltip>
-						</Tooltip.Provider>
-					</Form.Label>
+					<Form.Label className="flex min-w-44">{t("common.instrument")}</Form.Label>
 
 					<SearchInstrumentCombobox
 						className="w-full md:max-w-[350px] lg:max-w-[446px]"
