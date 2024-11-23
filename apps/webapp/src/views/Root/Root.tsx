2fc7cc313c1427b6143d3f7e26423b1995ab33d3
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-11-23 16:36:57 +0100

    remove legacy

--- apps/webapp/src/views/Root/Root.tsx
+++ apps/webapp/src/views/Root/Root.tsx
@@ -2,13 +2,9 @@ import { Toaster } from "@funds-tracker/ui";
 import { useTheme } from "next-themes";
 import { ComponentProps, FC, Suspense } from "react";
 import { useRoutes } from "react-router-dom";
-import { CashAccountsRoutes } from "views/CashAccounts";
 import { HomeRoutes } from "views/Home";
-import { HubRoutes } from "views/Hub";
 import { NotFoundRoutes } from "views/NotFound";
-import { PortfoliosRoutes } from "views/Portfolios";
 import { ResetPasswordRoutes } from "views/ResetPassword";
-import { SettingsRoutes } from "views/Settings";
 import { SinginRoutes } from "views/Signin";
 import { SignupRoutes } from "views/Signup";
 
@@ -20,10 +16,6 @@ export const Root: FC = () => {
 		...SinginRoutes,
 		...SignupRoutes,
 		...ResetPasswordRoutes,
-		...CashAccountsRoutes,
-		...PortfoliosRoutes,
-		...SettingsRoutes,
-		...HubRoutes,
 		...NotFoundRoutes,
 	]);
 
