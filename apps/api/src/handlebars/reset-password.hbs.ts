5287be6ca239c237f1d1fad31d43c87aa038fa81
Bartek Kochanowicz <bkochanovitz@gmail.com>
2025-01-27 18:37:19 +0100

    fix: reset password template

--- apps/api/src/handlebars/reset-password.hbs.ts
+++ apps/api/src/handlebars/reset-password.hbs.ts
@@ -10,37 +10,30 @@ export default `
 
   <body style="margin: 0;width: 100%;font-size: 16px;background-color: rgb(249, 249, 250);font-family: 'Roboto', sans-serif;color: rgb(52, 52, 52);">
     <div style="margin: 0 auto;max-width: 550px;width: 90%;">
-      <img src="https://www.funds-tracker.com/favicon.svg"  style="display:block;margin: 0 auto;width: 50px;height: auto;padding: 24px 0;" alt="Funds Tracker logo">
+        <img src="https://www.funds-tracker.com/favicon.svg"  style="display:block;margin: 0 auto;width: 50px;height: auto;padding: 24px 0;" alt="Funds Tracker logo">
 
-      <div style="display: block;margin: 0;color: rgb(255, 255, 255);font-size: 20px;padding: 24px;text-align: center;background-color: rgb(52, 52, 52);border-radius: 6px;margin-bottom: 4px;">Complete registration</div>
+        <div style="display: block;margin: 0;color: rgb(255, 255, 255);font-size: 20px;padding: 24px;text-align: center;background-color: rgb(52, 52, 52);border-radius: 6px;margin-bottom: 4px;">Reset Your Password</div>
 
-      <div style="margin: 0;margin-top: 8px;padding: 24px;background-color: rgb(255, 255, 255);border-radius: 6px;text-align: center;font-size: 14px;">
-        <p>Hello {{name}},</p>
-        <p style="margin: 0;display: block;text-align: center;font-size: 14px;">
-          <b style="margin: 0;">Please enter this confirmation code in the window
-          </b>
+        <div style="margin: 0;margin-top: 8px;padding: 24px;background-color: rgb(255, 255, 255);border-radius: 6px;text-align: center;font-size: 14px;">
+            <p>Hello {{name}},</p>
+            <p>
+              We received a request to reset your password for your <a class="link" href="https://funds-tracker.com">Funds Tracker</a> account. If you didn't make this request, please ignore this email.
+            </p>
+            <p>
+              To reset your password, click the link below:
+            </p>
+            <p style="text-align: center;word-break: break-all;">
+                <a href="{{passwordResetLink}}">{{passwordResetLink}}</a>
+            </p>
+            <p>
+              <b>This link will expire in 24 hours for security reasons.</b>
+            </p>
+            <p style="margin: 0;display: block;text-align: center;font-size: 12px;color: rgb(152, 161, 168);margin-top: 40px;">
+                If you have any questions or concerns, please don't hesitate to contact our support team at <a href="mailto:support@funds-racker.com">support@funds-tracker.com</strong>
+            </p>
+        </div>
 
-          where you started creating your account:
-        </p>
-
-        <span style="margin: 32px 0;display: block;width: 100%;padding: 24px;background-color: rgb(222, 225, 227);border-radius: 6px;font-weight: 600;font-size: 36px;text-align: center;box-sizing: border-box;">
-          {{code}}
-        </span>
-
-        <p style="margin: 0;display: block;text-align: center;font-size: 14px;margin-bottom: 8px;">
-          <b style="margin: 0;">Why do I have to confirm my email?</b>
-        </p>
-
-        <p style="margin: 0;display: block;text-align: center;font-size: 14px;margin-bottom: 8px;">
-          Confirming your email address certifies that you are the owner of this account. Without it, you not be able to use Funds Tracker until your account is confirmed.
-        </p>
-
-        <p style="margin: 0;display: block;text-align: center;font-size: 12px;color: rgb(152, 161, 168);margin-top: 40px;">
-          You received this email because you have signed up for <a href="https://funds-tracker.com/">Funds Tracker</a> - the platform to track and manage your funds.
-        </p>
-      </div>
-
-      <p style="display: block;text-align: center;font-size: 12px;color: rgb(152, 161, 168);margin-top: 20px;">
+        <p style="display: block;text-align: center;font-size: 12px;color: rgb(152, 161, 168);margin-top: 20px;">
             This email was sent to <a href="mailto:{{email}}" style="margin: 0;">{{email}}</a> because you are a registered Funds Tracker user.
         </p>
 
