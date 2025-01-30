b8dc06da4e87fbba197153e5461c539622855b66
Bartek Kochanowicz <bkochanovitz@gmail.com>
2025-01-30 17:21:04 +0100

    fix: update email confirmation

--- apps/api/src/handlebars/email-confirmation.hbs.ts
+++ apps/api/src/handlebars/email-confirmation.hbs.ts
@@ -10,11 +10,7 @@ export default `
 
   <body style="margin: 0;width: 100%;font-size: 16px;background-color: rgb(249, 249, 250);font-family: 'Roboto', sans-serif;color: rgb(52, 52, 52);">
     <div style="margin: 0 auto;max-width: 550px;width: 90%;">
-      <div style="margin: 0 auto;width: 400px;height: auto;">
-        <a href="https://funds-tracker.com/" style="margin: 0;">
-          <img src="https://www.funds-tracker.com/favicon.svg" style="margin: 0;width: 400px;height: auto;padding: 24px 0;" >
-        </a>
-      </div>
+      <img src="https://www.funds-tracker.com/favicon.svg"  style="display:block;margin: 0 auto;width: 50px;height: auto;padding: 24px 0;" alt="Funds Tracker logo">
 
       <div style="display: block;margin: 0;color: rgb(255, 255, 255);font-size: 20px;padding: 24px;text-align: center;background-color: rgb(52, 52, 52);border-radius: 6px;margin-bottom: 4px;">Complete registration</div>
 
